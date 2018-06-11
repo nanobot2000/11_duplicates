debf61e5207bbdbc25d3015226875bb51fdd7f2a
nanobot2000 <marginal3452@yandex.ru>
2018-06-11 23:18:10 +0300

    fixed length of code strings, variable names, etc.

--- duplicates.py
+++ duplicates.py
@@ -1,4 +1,5 @@
 import os
+import sys
 import argparse
 from collections import defaultdict
 
@@ -7,8 +8,10 @@ def find_duplicates(directory_path):
     files_info = defaultdict(list)
     for path, dirs, filemanes in os.walk(directory_path):
         for filename in filemanes:
-            files_info[(filename, os.stat(os.path.join(path, filename)).st_size)].append(path)
-    return {file_info: paths for file_info, paths in files_info.items() if len(paths) > 1}
+            files_info[(filename,
+                        os.stat(os.path.join(path, filename)).st_size)].append(path)
+    return {file_info: paths for file_info, paths
+            in files_info.items() if len(paths) > 1}
 
 
 def create_argparser():
@@ -27,18 +30,18 @@ if __name__ == '__main__':
     argparser = create_argparser()
     directory_path = argparser.directory
     if not os.path.isdir(directory_path):
-        print('Directory {} doesn\'t exist! Try another directory!'.format(directory_path))
-        exit()
+        sys.exit("Directory {} doesn't exist! Try another directory!"
+                 .format(directory_path))
     else:
         duplicates = find_duplicates(directory_path)
     if duplicates:
         print('Duplicates found: ')
         print('-----------------------')
         for file_info, paths in duplicates.items():
-            filename, size = file_info
-            print('File with name {} is repeated in the following folders:'.format(filename))
-            for path in paths:
-                print(path)
+            filename, _ = file_info
+            print('File with name {} is repeated in the following folders:'
+                  .format(filename))
+            print('\n'.join(paths))
         print('_______________________')
     else:
         print('No duplicates found')
